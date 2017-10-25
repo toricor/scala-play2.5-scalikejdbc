30f299a1854dceeb3c0f3f98412ff3916445416c
toriyabe <toriyabe@mfpc243.local>
2017-10-25 16:28:16 +0900

    list, editを実装(ただしエラーでうごかない)

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -9,15 +9,53 @@ import scalikejdbc._
 import models._
 
 class UserController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
   /**
     * 一覧表示
     */
-  def list = TODO
+  def list = Action { implicit request =>
+    val u = Users.syntax("u")
+
+    DB.readOnly { implicit session =>
+      // ユーザのリストを取得
+      val users = withSQL {
+        select.from(Users as u).orderBy(u.id.asc)
+      }.map(Users(u.resultName)).list.apply()
+
+      // 一覧画面を表示
+      Ok(views.html.user.list(users))
+    }
+  }
 
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  // コンパニオンオブジェクトに定義したFormを参照するためにimport文を追加
+  import UserController._
+  def edit(id: Option[Long]) = Action { implicit request =>
+    val c = Companies.syntax("c")
+
+    DB.readOnly { implicit session =>
+      // リクエストパラメータにIDが存在する場合
+      val form = id match {
+        // IDが渡されなかった場合は新規登録フォーム
+        case None => userForm
+        // IDからユーザ情報を1件取得してフォームに詰める
+        case Some(id) => {
+          val user = Users.find(id).get
+          userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+        }
+      }
+
+      // プルダウンに表示する会社のリストを取得
+      val companies = withSQL {
+        select.from(Companies as c).orderBy(c.id.asc)
+      }.map(Companies(c.resultName)).list().apply()
+
+      Ok(views.html.user.edit(form, companies))
+    }
+
+  }
 
   /**
    * 登録実行
@@ -34,4 +72,18 @@ class UserController @Inject()(val messagesApi: MessagesApi) extends Controller 
     */
   def remove(id: Long) = TODO
 
+}
+
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
 }
